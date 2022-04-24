2bcb4e53c9c1e4088d6d7d51c647bfb60a78804b
gbedad <gerald.berrebi@gmail.com>
2022-04-24 22:16:56 +0200

    daily writing with stars

--- week_5/day_5/daily_challenge/main.js
+++ week_5/day_5/daily_challenge/main.js
@@ -34,16 +34,46 @@ let checkStar = (value) => {
       return '*';
   }
 };
+
+const matrix = document.querySelector('.grid-container');
+// const div = document.createElement('div');
+// let createCell = (i, j) => {
+//   let idCell = document.createElement('td');
+//   idCell.textContent = checkStar(k);
+//   idCell.classList.add('tdCell');
+//   // tr.append(idCell);
+//   return idCell;
+// };
+// console.log(createCell(2));
+
+// let cells = [...document.querySelectorAll('.grid-item')];
+
 const writingLetter = (arrayOfLetter) => {
   for (let i = 0; i < arrayOfLetter.length; i++) {
-    let line = '';
+    // let line = '';
+
     for (let j = 0; j < arrayOfLetter[i].length; j++) {
-      line = line + checkStar(arrayOfLetter[i][j]);
+      // let div = document.createElement('div');
+      let cell = document.createElement('div');
+      matrix.appendChild(cell);
+      cell.classList.add('grid-item');
+      cell.textContent = checkStar(arrayOfLetter[i][j]);
+      console.log(i, j);
+      // let cell = document.createTextNode(createCell(arrayOfLetter[i][j]))
+
+      // cells.classList.add('grid-item');
+      // cells.textContent = createCell(arrayOfLetter[i][j]);
+      // line = line + checkStar(arrayOfLetter[i][j]);
+      // cells[j].createTextNode(checkStar(arrayOfLetter[i][j]));
+      // let td = createCell(arrayOfLetter[i][j]);
+      // tr.append(td);
     }
-    console.log(line);
+
+    // table.append(tr);
+    // console.log(line);
   }
 };
 
 writingLetter(composeA);
-writingLetter(composeB);
-writingLetter(composeS);
+// writingLetter(composeB);
+// writingLetter(composeS);
