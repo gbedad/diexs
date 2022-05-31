8cf8eef7c36964b149ac9cef326d83223f5c75c2
gbedad <gerald.berrebi@gmail.com>
2022-05-31 11:16:28 +0200

    Generating password done ex_gold d2/w10

--- week_10/day_2/exercise_gold.py
+++ week_10/day_2/exercise_gold.py
@@ -1,4 +1,5 @@
 # Exercise 1 Regular expression 1
+import random
 import re
 
 
@@ -15,7 +16,7 @@ user_name = input("\nEnter your fullname as Name Lastname: ")
 
 def check_name_validity(name):
 
-    pattern = re.match(r'^[A-Z][a-z]+\s+^[A-Z][a-z]+', name)
+    pattern = re.match(r'^[A-Z][a-z]\^[A-Z][a-z]', name)
     if pattern:
         return True
     else:
@@ -25,3 +26,50 @@ def check_name_validity(name):
 print(check_name_validity(user_name))
 
 # Exercise 3: Python Password Generator
+
+
+def test_password(password):
+    reg = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-~'`=]).{6,30}$"
+    pattern = re.compile(reg)
+    valid_password = re.search(pattern, password)
+    if valid_password:
+        return "The password is valid"
+    else:
+        return "Invalid password"
+
+
+def set_password_length():
+    v_length = False
+    while not v_length:
+        user_input = input("Enter password length (6-30): ")
+        if not re.match(r'\d+', user_input):
+            v_length = False
+        elif 6 <= int(user_input) <= 30:
+            v_length = True
+
+    return user_input
+
+
+def main(a):
+    # length = int(set_password_length())
+    generated_password = ''
+    for i in range(a):
+        c = chr(random.randint(33, 123))
+        if 91 <= i <= 95:
+            continue
+        generated_password += c
+    return generated_password
+
+
+# test = main()
+# print(test)
+
+# print(test_password(password=test))/
+
+for i in range(100):
+    for j in range(6, 31):
+        test = main(j)
+        print(test)
+        print(test_password(test))
+
+
