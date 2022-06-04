cbb385eadba6a22433314df3bd43c7d4556ff000
gbedad <gerald.berrebi@gmail.com>
2022-06-04 19:03:33 +0200

    Print function in class Game, still to create files json

--- week_10/day_4/exercise_ninja/dungeon_and_dragons.py
+++ week_10/day_4/exercise_ninja/dungeon_and_dragons.py
@@ -3,6 +3,7 @@ from random import randint
 
 class Character:
     count_characters = 0
+    characters_list = []
 
     def __init__(self, name, age):
         self.name = name
@@ -14,6 +15,7 @@ class Character:
         self.wisdom = self.discard_min_and_sum(self.trow_dices())
         self.charisma = self.discard_min_and_sum(self.trow_dices())
         Character.count_characters += 1
+        Character.characters_list.append(self)
 
     @classmethod
     def discard_min_and_sum(cls, arr):
@@ -30,13 +32,38 @@ class Character:
             dices.append(dice)
         return dices
 
+    def __str__(self):
+        text = f"name: {self.name}\nage: {self.age}\nIntelligence: {self.intelligence}\nDexterity: {self.dexterity}\nConstitution: {self.constitution}\nWisdom: {self.wisdom}\nCharisma: {self.charisma}"
+        return text
 
-class Game(Character):
+
+class Game:
+
     def __init__(self):
-        self.count = Character.count_characters
+        self.characters = Character.characters_list
 
-    def show_how_many(self):
-        print(self.count)
+    def __str__(self):
+        return Character.__str__(self)
+
+    @staticmethod
+    def show_how_many():
+        print(f"Total number of players created: {Character.count_characters}")
+
+    def characters_game(self):
+        print("Characters of the Game\n----------------------")
+
+        for item in self.characters:
+
+            print(item)
+            print("------------------")
+
+
+
+
+
+
+
+
 
 
 player1 = Character("Joe", 24)
@@ -45,6 +72,5 @@ player3 = Character("Jane", 22)
 player4 = Character("Junior", 44)
 
 game = Game()
-print(game.count_characters)
-
-print(player1.intelligence)
+game.show_how_many()
+game.characters_game()
