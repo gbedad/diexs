d301e37e79a152d2b354fa45650e6f9a4e900336
gbedad <gerald.berrebi@gmail.com>
2022-06-05 10:46:29 +0200

    Add save to json and txt

--- week_10/day_4/exercise_ninja/dungeon_and_dragons.py
+++ week_10/day_4/exercise_ninja/dungeon_and_dragons.py
@@ -1,3 +1,4 @@
+import json
 from random import randint
 
 
@@ -5,7 +6,7 @@ class Character:
     count_characters = 0
     characters_list = []
 
-    def __init__(self, name, age):
+    def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
         self.strength = self.discard_min_and_sum(self.trow_dices())
@@ -50,27 +51,52 @@ class Game:
         print(f"Total number of players created: {Character.count_characters}")
 
     def characters_game(self):
-        print("Characters of the Game\n----------------------")
+        file_txt = 'players.txt'
+        with open(file_txt, 'w+') as f:
+            f.seek(0)
+            print("Characters of the Game\n----------------------")
+            for item in self.characters:
+                f.writelines(str(item)+'\n')
+                print(item)
+                print("------------------")
 
+    def save_to_file(self):
         for item in self.characters:
-
             print(item)
-            print("------------------")
+
+
+def main():
+    user_input = int(input("How many players: "))
+    players_dict ={}
+    for i in range(user_input):
+        print(f"Enter player {i+1}")
+        player_name = input("Player name: ")
+        player_age = int(input("Player age: "))
+        player_i = Character(player_name, player_age)
+        players_dict['player'+str(i+1)] = vars(player_i)
+    # print(players_dict)
+    filename = 'players.json'
+    with open(filename, 'w') as f:
+        json.dump(players_dict, f, indent=4)
+
+    game = Game()
+    return game
+
+
+my_game = main()
+
+
+my_game.characters_game()
 
 
 
 
-
-
-
-
-
-
-player1 = Character("Joe", 24)
-player2 = Character("James", 32)
-player3 = Character("Jane", 22)
-player4 = Character("Junior", 44)
-
-game = Game()
-game.show_how_many()
-game.characters_game()
+# player1 = Character("Joe", 24)
+# player2 = Character("James", 32)
+# player3 = Character("Jane", 22)
+# player4 = Character("Junior", 44)
+#
+# game = Game()
+# game.show_how_many()
+# game.characters_game()
+# game.save_to_file()
