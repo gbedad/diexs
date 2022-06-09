d08ac976be57386bd16d4e2f698cfded83b7e061
gbedad <gerald.berrebi@gmail.com>
2022-06-09 14:38:29 +0200

    Search functions of xp_gold, style css still to finish

--- week_11/day_4/exercise_xp/main.py
+++ week_11/day_4/exercise_xp/main.py
@@ -20,21 +20,37 @@ def products_page():
     return render_template("products.html", products=data)
 
 
-@app.route("/product/<product_id>")
+@app.route("/products/<product_id>")
 def product_page(product_id):
     data = database_manager.load_database()
+    filtered = filter(lambda x: x["ProductId"] == product_id, data)
+    product = list(filtered)[0]
 
-    for item in data:
-        if item['ProductId'] == product_id:
-            product = item
-            break
-    else:
-        product = None
+    # for item in data:
+    #     if item['ProductId'] == product_id:
+    #         product = item
+    #         break
+    # else:
+    #     product = None
 
     # css = open('static/style.css', 'r').read()
     # return flask.render_template("my_template.html", products=data, additional_css=css)
 
     return render_template("product.html", product=product)
+
+
+@app.route('/products/search/by-category/<string:category_name>')
+def search_by_category(category_name):
+    data = database_manager.load_database()
+    filtered = filter(lambda x: x["Category"] == category_name, data)
+    return render_template('search-by-category.html', products=list(filtered), category=category_name)
+
+
+@app.route('/products/search/by-name/<string:product_name>')
+def search_by_name(product_name):
+    data = database_manager.load_database()
+    filtered = filter(lambda x: product_name in x["Name"], data)
+    return render_template('search-by-name.html', products=list(filtered), name=product_name)
 
 
 if __name__ == "__main__":
