f1add1e467c9351c6236362404c824737fa411c0
gbedad <gerald.berrebi@gmail.com>
2022-07-04 12:39:47 +0200

    Create Models and sqlite database

--- week_15/day_2/review_project_imdi/FilmProject/app/__init__.py
+++ week_15/day_2/review_project_imdi/FilmProject/app/__init__.py
@@ -1,6 +1,17 @@
 import flask
+import flask_sqlalchemy
+import flask_migrate
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 from app.film import film_blueprint
 
 flask_app = flask.Flask(__name__)
+
+flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'flask_app.db')
+
+db = flask_sqlalchemy.SQLAlchemy(flask_app)
+migrate = flask_migrate(flask_app, db)
+
---
+++ week_15/day_2/review_project_imdi/FilmProject/app/film/models.py
@@ -0,0 +1,60 @@
+
+from app import db
+from datetime import date
+
+
+country_table = db.Table('country',
+                         db.Column('film_id', db.Integer, db.ForeignKey('film.id)')),
+                         db.Column('country_id', db.Integer, db.ForeignKey('country.id')))
+
+
+category_table = db.Table('category',
+                          db.Column('film_id', db.Integer, db.ForeignKey('film.id)')),
+                          db.Column('category_id', db.Integer, db.ForeignKey('category.id')))
+
+
+director_table = db.Table('director',
+                          db.Column('film_id', db.Integer, db.ForeignKey('film.id)')),
+                          db.Column('director_id', db.Integer, db.ForeignKey('director.id')))
+
+
+class Country(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(64))
+    films = db.relationship('Film', backref='country', lazy='dynamic')
+
+    def __repr__(self):
+        return f'<Country: {self.name}'
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(64))
+
+    def __repr__(self):
+        return f'<Category: {self.name}'
+
+
+class Film(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(64))
+    release_date = db.Column(db.Datetime, default=date.today())
+    created_in_country = db.Column(db.String(64), db.ForeignKey('country.id'))
+    available_in_countries = db.relationship('Country', secondary=country_table)
+    category = db.relationship('Category', secondary=category_table)
+    director = db.relationship('Director', secondary=director_table)
+
+    def __repr__(self):
+        return f'<Film: {self.title}'
+
+
+class Director(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String(64))
+    last_name = db.Column(db.String(64), index=True)
+
+    def __repr__(self):
+        return f'<Director: {self.first_name} {self.last_name}'
+
+
+
