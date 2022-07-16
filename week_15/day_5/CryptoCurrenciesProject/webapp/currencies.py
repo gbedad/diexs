336e17b1f73ce8d3b405f68f049c0c4557a17129
gbedad <gerald.berrebi@gmail.com>
2022-07-16 12:18:25 +0200

    Set schedule package to run currency.py daily

--- week_15/day_5/CryptoCurrenciesProject/webapp/currencies.py
+++ week_15/day_5/CryptoCurrenciesProject/webapp/currencies.py
@@ -4,54 +4,53 @@ from webapp.models import Cryptocurrencies, User
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
+import schedule
+import time
+
+def set_crypto_in_db():
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map'
+    parameters = {
+        'start': '1',
+        'limit': '25',
+        'sort': 'cmc_rank'
+    }
+    headers = {
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': '91f90390-6831-4e62-b62e-824b6d76ae02',
+    }
+
+    session = Session()
+    session.headers.update(headers)
+
+    try:
+        response = session.get(url, params=parameters)
+        data = json.loads(response.text)
+        all_currencies = data["data"]
+        print(all_currencies)
+    except (ConnectionError, Timeout, TooManyRedirects) as e:
+        print(e)
 
 
+    for currency in all_currencies:
+        c = Cryptocurrencies.query.filter_by(id=currency["id"]).first()
+        if c is None:
+            zulu = Cryptocurrencies(id=currency["id"], name=currency["name"], symbol=currency["symbol"], slug=currency["slug"],
+                                    first_historical_data=currency["first_historical_data"],
+                                    last_historical_data=currency["last_historical_data"], is_active=currency["is_active"] )
 
-
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map'
-parameters = {
-    'start': '1',
-    'limit': '25',
-    'sort': 'cmc_rank'
-}
-headers = {
-    'Accepts': 'application/json',
-    'X-CMC_PRO_API_KEY': '91f90390-6831-4e62-b62e-824b6d76ae02',
-}
-
-session = Session()
-session.headers.update(headers)
-
-try:
-    response = session.get(url, params=parameters)
-    data = json.loads(response.text)
-    all_currencies = data["data"]
-    print(all_currencies)
-except (ConnectionError, Timeout, TooManyRedirects) as e:
-    print(e)
-
-
-for currency in all_currencies:
-    c = Cryptocurrencies.query.filter_by(id=currency["id"]).first()
-    if c is None:
-        zulu = Cryptocurrencies(id=currency["id"], name=currency["name"], symbol=currency["symbol"], slug=currency["slug"],
-                                first_historical_data=currency["first_historical_data"],
-                                last_historical_data=currency["last_historical_data"], is_active=currency["is_active"] )
-
-        db.session.add(zulu)
-    else:
-        c.name = currency["name"]
-        c.symbol = currency["symbol"]
-        c.slug = currency["slug"]
-        c.first_historical_data = currency["first_historical_data"]
-        c.last_historical_data = currency["last_historical_data"]
-db.session.commit()
-
-cryptos_in_db = Cryptocurrencies.query.all()
-url_metadata_v2 = 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/info'
+            db.session.add(zulu)
+        else:
+            c.name = currency["name"]
+            c.symbol = currency["symbol"]
+            c.slug = currency["slug"]
+            c.first_historical_data = currency["first_historical_data"]
+            c.last_historical_data = currency["last_historical_data"]
+    db.session.commit()
 
 
 def add_logo():
+    cryptos_in_db = Cryptocurrencies.query.all()
+    url_metadata_v2 = 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/info'
     for crypto in cryptos_in_db:
         crypto_id = crypto.id
         print(crypto_id)
@@ -70,12 +69,16 @@ def add_logo():
             data = json.loads(response.text)["data"]
         except (ConnectionError, Timeout, TooManyRedirects) as e:
             print(e)
-        print(data[str(crypto_id)]["logo"])
         crypto.logo = data[str(crypto_id)]["logo"]
 
         db.session.commit()
 
 
-add_logo()
+schedule.every().day.at("00:00").do(set_crypto_in_db)
+schedule.every().day.at("00:30").do(add_logo)
+
+while True:
+    schedule.run_pending()
+    time.sleep(2)
 
 
